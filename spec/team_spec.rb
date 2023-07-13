ac2f6057653df1c2a81986a6e476cc8fda17d55f
Terry Lamb <terrylamb@gmail.com>
2023-07-13 12:11:01 -0400

    create team_spec and team.rb files and work through interaction pattern

---
+++ spec/team_spec.rb
@@ -0,0 +1,23 @@
+require './lib/player'
+require './lib/team'
+
+RSpec.describe Team do
+  it "exists and has readable attributes" do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team.roster).to eq([])
+    expect(team.player_count).to eq(0)
+  end
+  it "has players on the roster" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+
+    require 'pry'; binding.pry
+    expect(team.roster).to be_a([])
+    expect(team.player_count).to eq(2)
+  end
+end
